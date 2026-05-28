200bfb60079a522fc6e2d9143ee5b04b67ecb88d
kristhianvilla15-ui <kristhianvilla15@gmail.com>
2026-05-28 15:24:00 -0500

    Corregida contraseña y añadido SSL para conexión a Aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,18 @@
-const mysql = require('mysql2'); //importamos el traductor para hablar con mysql
-require('dotenv').config(); //carga las variables del .env
-
-//configuramos un grupo de conexiones (pool) para que el API sea mas rapido
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,//la direccion 
-    user: process.env.DB_USER,//Usuario root
-    password: process.env.DB_PASSWORD,//contraseña
-    database: process.env.DB_NAME,//nombre de la db
-
-    waitForConnections: true, // si esta lleno, espera a que se libere una conexion
-    connectionLimit: 10 //maximo 10 conexiones simultaneas
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,   // Añade esta línea (aunque no es estrictamente necesaria si usas el default)
+    ssl: {
+        ca: fs.readFileSync('./ca.pem')
+    },
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
-//exportamos el pool usando promesas para poder usar await en otros archivos
 module.exports = pool.promise();
